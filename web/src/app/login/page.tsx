3ee018c48867232b00b886d46ec978bb02151670
Bruno Medeiros <bruno.med.oliv@gmail.com>
2023-11-06 21:19:37 -0300

    Fix:Consertando Home

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,20 +1,20 @@
+import Image from 'next/image'
+import Logo from '../../assets/logo.svg'
+
 export default function Login() {
   return (
-    <div className="bg-[#F2F2F7] w-screen h-screen justify-center items-center flex">
-      <div className="bg-[#FFF] w-5/12 h-3/5 rounded-3xl flex flex-col items-center shadow-xl justify-between">
-        <div className="flex self-start mt-8 ml-8">
-          <p className="font-semibold text-[#7839CD] text-2xl">Tabeçai</p>
-          <img></img>
+    <div className="flex bg-[#cae3cc] justify-center items-center h-screen">
+      <div className="md:w-6/12 p-10 bg-white shadow-xl rounded-2xl">
+        <div className="flex flex-row items-center"> 
+          <p className="text-2xl text-[#422F8A] font-bold mr-2">Tabeçai</p>
+          <Image src={Logo} alt="Logo" width={22} height={22}/>
         </div>
-        <h1 className="font-semibold text-3xl">Login</h1>
-        <form className="flex flex-col gap-4 mb-40 items-center">
-          <input type="text" className="bg-[#D9D9D9] border-2 rounded-lg text-[#000000]" placeholder="Usuário " />
-          <input type="password" className="bg-[#D9D9D9] border-2 rounded-lg" placeholder="Senha"/>
-          <button className="bg-[#7839CD] font-bold text-[#FFF] rounded-xl p-2 w-3/5 text mt-7">
-            Entrar
-          </button>
+        <h1 className="text-3xl mt-20 mb-24 text-center font-bold">Login</h1>
+        <form className="flex flex-col justify-center items-center gap-6" action="/login" method="post">
+          <input type="text" className="w-10/12 md:w-8/12 px-4 py-2.5 mb-4 border-transparent bg-[#EEEEEE] rounded-lg shadow-lg placeholder-gray-500" placeholder="Usuário"></input>
+          <input type="password" className="w-10/12 md:w-8/12 px-4 py-2.5 border-transparent bg-[#EEEEEE] rounded-lg shadow-lg placeholder-gray-500" placeholder="Senha"></input>
+          <button className="w-1/2 h-12 mt-4 bg-[#7839CD] text-white rounded-lg font-bold shadow-xl" type="submit">Entrar</button>
         </form>
-
       </div>
     </div>
   )
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,18 +4,7 @@ import Logo from '../assets/logo.svg'
 export default function Home() {
   return (
     <div className="flex bg-[#cae3cc] justify-center items-center h-screen">
-      <div className="md:w-6/12 p-10 bg-white shadow-xl rounded-2xl">
-        <div className="flex flex-row items-center"> 
-          <p className="text-2xl text-[#422F8A] font-bold mr-2">Tabeçai</p>
-          <Image src={Logo} alt="Logo" width={22} height={22}/>
-        </div>
-        <h1 className="text-3xl mt-20 mb-24 text-center font-bold">Login</h1>
-        <form className="flex flex-col justify-center items-center gap-6" action="/login" method="post">
-          <input type="text" className="w-10/12 md:w-8/12 px-4 py-2.5 mb-4 border-transparent bg-[#EEEEEE] rounded-lg shadow-lg placeholder-gray-500" placeholder="Usuário"></input>
-          <input type="password" className="w-10/12 md:w-8/12 px-4 py-2.5 border-transparent bg-[#EEEEEE] rounded-lg shadow-lg placeholder-gray-500" placeholder="Senha"></input>
-          <button className="w-1/2 h-12 mt-4 bg-[#7839CD] text-white rounded-lg font-bold shadow-xl" type="submit">Entrar</button>
-        </form>
-      </div>
+      <h1 className="text-3xl mt-20 mb-24 text-center font-bold">Home</h1>
     </div>
   )
 }
