ffd4b873b38ee083abfb3f550a9245607d96f54d
Maria Luisa <211039617@aluno.unb.br>
2023-11-15 12:20:24 -0300

    feat: add componentes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,54 @@
-import Image from 'next/image'
-import Logo from '../assets/logo.svg'
+import { ChevronDown, DownloadCloud, Filter, Moon, MoreHorizontal, MoreVertical, Search } from "lucide-react"
+import Image from "next/image"
+import Logo from "../../assets/logo.svg"
+import { Header } from "@/components/Header"
+import { Table } from "@/components/Table"
 
-export default function Home() {
-  return (
-    <div className="flex bg-[#cae3cc] justify-center items-center h-screen">
-      <h1 className="text-3xl mt-20 mb-24 text-center font-bold">Home</h1>
-    </div>
-  )
+const filtros = ["Tudo", "Efetuado", "Em Processo", "Transação Com Erro"]
+
+export default function Login() {
+    return (
+        <div className="h-screen flex flex-col">
+            <Header />
+
+            <main className="">
+                <div className="flex flex-row items-end justify-between">
+                    <strong className="text-2xl m-10">Caixa</strong>
+                    <button className="flex flex-row space-x-2 items-center p-4 text-white bg-indigo-600 rounded-lg m-10">
+                        <strong className="text-white">Adicionar</strong>
+                        <DownloadCloud size={24} />
+                    </button>
+                </div>
+
+                <div className="flex flex-row justify-between items-end">
+                    <div className="flex flex-row ml-6 items-baseline">
+                        {filtros.map((filtro) => {
+                            return (
+                                <div className="items-center justify-between px-4 pt-4 " key={filtro}>
+                                    <p className="text-base text-zinc-500">{filtro}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="flex flex-row space-x-4 items-end pr-10">
+                        <div className="flex flex-row space-x-2 items-center p-4 text-indigo-600 border-2 border-indigo-600  rounded-lg">
+                            <Search size={24} />
+                            <input type="text" />
+                        </div>
+                        <button className="flex flex-row space-x-2 p-4 text-indigo-600 border-2 border-indigo-600 bg-indigo-200 rounded-lg">
+                            <strong className="text-indigo-600">Filtro</strong>
+                            <Filter size={24} />
+                        </button>
+                        <button className="flex flex-row space-x-2 p-4 text-indigo-600 border-2 border-zinc-300 bg-indigo-200 rounded-lg ">
+                            <MoreVertical size={24} />
+                        </button>
+                    </div>
+                </div>
+                <div className="px-10 pt-2 pe-10" >
+                    <Table />
+                </div>
+
+            </main>
+        </div>
+    )
 }
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,58 @@
+import { ChevronDown, Moon, MoonIcon, Search } from "lucide-react"
+import Image from "next/image"
+import Logo from "../assets/logo.svg"
+
+
+
+const telas = [{
+    nome: "Caixa1",
+    http: ""
+},
+{
+    nome: "Caixa2",
+    http: ""
+},
+{
+    nome: "Caixa3",
+    http: ""
+},
+{
+    nome: "Caixa4",
+    http: ""
+}]
+
+const filtros = ["Tudo", "Efetuado", "Em Processo", "Transação Com Erro"]
+
+export const Header = () => {
+    return (<header className="flex flex-row items-center p-6 justify-between border-b-2 border-gray-200">
+    <div className="flex flex-row items-center justify-betweenspace-x-2">
+        <strong className="text-4xl text-[#422F8A] justify-center font-bold mr-3">Tabeçai</strong>
+        <Image src={Logo} alt="Logo" width={22} height={22} />
+
+        <div className="flex space-x-6 ml-6">
+            {telas.map((tela) => {
+                return (
+                    <div className="" key={tela.nome} >
+                        <p className="text-xl text-zinc-500">{tela.nome}</p>
+                    </div>
+                )
+            })}
+            <div className="flex  flex-row space-x-3">
+                <p className=" text-zinc-300">|</p>
+                <Search size={24} />
+                {/* <input type="text" className='flex bg-green-700' /> */}
+            </div>
+
+        </div>
+    </div>
+
+    <div className="flex flex-row space-x-6 items-center">
+        <strong className="">UsuarioADM</strong>
+        <div className='bg-green-500 h-12 w-12 flex items-center  justify-center pl-1 rounded-full ml-auto '>
+            {/* footo do  user */}
+        </div>
+        <ChevronDown size={20} />
+        <MoonIcon size={20} />
+    </div>
+</header>)
+}
