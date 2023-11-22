47d96273f2485b5200daa6defd8760e8322c3602
Maria Luisa <211039617@aluno.unb.br>
2023-11-22 14:37:24 -0300

    feat: modal createFuncionario

--- web/src/components/ModalCreateFuncionario.tsx
+++ web/src/components/ModalCreateFuncionario.tsx
@@ -19,34 +19,9 @@ const customStyles = {
   }
 
   
-  
-  function ModalFodinha() {
-    return <h1>Foda Fodinha</h1>
-  }
-  
-  function ModalFodao() {
-    return <h1>Foda Fodao</h1>
-  }
-  
-  function ModalFodaoPraCaralho() {
-    return <h1>Foda Pra Caralho</h1>
-  }
-  
-//   const modals: Record<HomeProps["type"], React.FC> = {
-//     "Fodao pra caralho": ModalFodaoPraCaralho,
-//     "Fodao": ModalFodao,
-//     "Fodinha": ModalFodinha
-//   }
-  
-//   export default function Home({ type }: HomeProps) {
-//     const Component = modals[type]
-//     return <Component></Component>
-//   }
-  
-  export const ModalCreateFuncionario = ( {ModalType, onCloseModal} : 
-    ModalCreateFuncionarioProps) => {
+  export const ModalCreateFuncionario = ( {ModalType, onCloseModal} : ModalCreateFuncionarioProps) => {
         return(
-    <div>
+    <div className="m-100">
       
       <Modal
         isOpen={ModalType}
@@ -54,15 +29,12 @@ const customStyles = {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        
-            
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                
+            <div className="bg-white rounded-lg shadow dark:bg-gray-700">
                 <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                     <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                        Sign in to our platform
+                        Adicionar Novo Funcionario
                     </h3>
-                    <button type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+                    <button onClick={onCloseModal} type="button" className="end1.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                         </svg>
@@ -72,27 +44,49 @@ const customStyles = {
                 
                 <div className="p-4 md:p-5">
                     <form className="space-y-4" action="#">
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                        <div className="flex p-2 justify-center gap-4">
+                          <div>
+                            <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Primeiro Nome</label>
+                            <input type="text" name="firstName" id="firstName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nome" required />
+                          </div>
+                          <div>
+                            <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sobrenome</label>
+                            <input type="text" name="lastName" id="lastName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Sobrenome" required />
+                          </div>
                         </div>
                         <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                         
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                          
                         </div>
+
+                        <div>
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        </div>
+
+                        <div>
+                            <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">CPF</label>
+                            <input type="text" name="cpf" id="cpf" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        </div>
+
+                        <div>
+                            <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Data de Nascimento</label>
+                            <input type="date" name="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required />
+                        </div>
+                        
                         <div className="flex justify-between">
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
-                                    <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                                    <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-violet-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-violet-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
                                 </div>
-                                <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                                <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Administrador</label>
                             </div>
-                            <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                         </div>
-                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                            Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-                        </div>
+                        
+                        <button type="submit" className="w-full text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800">Criar</button>
+                        
                     </form>
                 </div>
             </div>
