45ef8e9f5e63abd454312aa2025fe2285e3fdeb9
Gustavo Henrique <gustavohenriqueprivado@gmail.com>
2023-11-23 00:36:12 -0300

    fix: add props to AlertComponent

--- web/src/components/Alert.tsx
+++ web/src/components/Alert.tsx
@@ -1,7 +1,9 @@
 import Modal from "react-modal";
 
 interface AlertProps {
-  message: string;
+  Message: string;
+  ModalType: boolean;
+  onCloseModal: () => void;
 }
 const customStyles = {
   content: {
@@ -11,17 +13,32 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
-    borderColor: "#000",
+    borderColor: "#DBDBDB",
     borderRadius: "10px",
   },
 };
-export const Alert = (props: AlertProps) => {
+export const AlertComponent = ({
+  Message,
+  ModalType,
+  onCloseModal,
+}: AlertProps) => {
   return (
-    <Modal isOpen style={customStyles}>
-    <div className="bg-[#ffffff] px-8 py-8 rounded-lg">
-      <h1 className="text-xl text-center font-semibold text-gray-900 dark:text-white">{props.message}</h1>
-      <button className=" mt-12 w-full text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-bold rounded-lg text-md px-5 py-2.5 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800">OK</button>
-    </div>
+    <Modal
+      isOpen={ModalType}
+      onRequestClose={onCloseModal}
+      style={customStyles}
+    >
+      <div className="bg-[#ffffff] px-8 py-8 rounded-lg">
+        <h1 className="text-xl text-center font-semibold text-gray-900 dark:text-white">
+          {Message}
+        </h1>
+        <button
+          onClick={onCloseModal}
+          className=" mt-12 w-full text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-bold rounded-lg text-md px-5 py-2.5 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800"
+        >
+          OK
+        </button>
+      </div>
     </Modal>
   );
 };
