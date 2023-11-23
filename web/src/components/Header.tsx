ea6222ce495a32f8587fa3cfe733e106d4430e7d
Gustavo Henrique <gustavohenriqueprivado@gmail.com>
2023-11-22 23:03:47 -0300

    fix: corrections to table

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -6,42 +6,42 @@ import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
 const telas = [
-    {
-        nome: "Caixa",
-        http: "/",
-    },
-    {
-        nome: "Clientes",
-        http: "/",
-    },
-    // {
-    //     nome: "Produto",
-    //     http: "/",
-    // },
-    {
-        nome: "Painel Financeiro",
-        http: "/",
-    },
-    {
-        nome: "Funcionario",
-        http: "/funcionarios",
-    },
+  {
+    nome: "Caixa",
+    http: "/",
+  },
+  // {
+  //     nome: "Clientes",
+  //     http: "/",
+  // },
+  // {
+  //     nome: "Produto",
+  //     http: "/",
+  // },
+  {
+    nome: "Painel Financeiro",
+    http: "/",
+  },
+  {
+    nome: "Funcionario",
+    http: "/funcionarios",
+  },
 ];
 
 const filtros = ["Tudo", "Efetuado", "Em Processo", "Transação Com Erro"];
 
 export const Header = () => {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const isMobile = useMediaQuery({ minWidth: 1280 });
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const isMobile = useMediaQuery({ minWidth: 1280 });
 
-    return (
-        <header className="flex flex-row items-center p-6 border-b-2 border-gray-200 w-full">
-            <div className="flex flex-row items-center">
-                <strong className="text-xl xl:text-4xl text-[#422F8A] justify-center font-bold mr-3">
-                    Tabeçai
-                </strong>
-                <Image src={Logo} alt="Logo" width={22} height={22} />
-                {/* <button
+  return (
+    <header className="flex flex-row items-center p-6 border-b-2 border-gray-200 w-full">
+      <div className="flex flex-row items-center">
+        <strong className="text-xl xl:text-4xl text-[#422F8A] justify-center font-bold mr-3">
+          Tabeçai
+        </strong>
+        <Image src={Logo} alt="Logo" width={22} height={22} />
+        {/* <button
                     id="dropdownDefaultButton"
                     data-dropdown-toggle="dropdown"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -64,32 +64,32 @@ export const Header = () => {
                         />
                     </svg>
                 </button> */}
-                {telas.map((tela) => {
-                    return (
-                        <a
-                            key={tela.nome}
-                            className="hover:font-bold cursor-pointer hover:bg-slate-100 rounded-xl text-sm md:text-lg p-2 sm:p-4 md:ml-4"
-                            href={tela.http}
-                        >
-                            {tela.nome}
-                        </a>
-                    );
-                })}
+        {telas.map((tela) => {
+          return (
+            <a
+              key={tela.nome}
+              className="hover:font-bold cursor-pointer hover:bg-slate-100 rounded-xl text-sm md:text-lg p-2 sm:p-4 md:ml-4"
+              href={tela.http}
+            >
+              {tela.nome}
+            </a>
+          );
+        })}
 
-                {/* <div className="flex flex-row space-x-3">
+        {/* <div className="flex flex-row space-x-3">
                     <p className=" text-zinc-300">|</p>
                     {/* <input type="text" className='flex bg-green-700' /> */}
-                {/* </div> */}
-            </div>
+        {/* </div> */}
+      </div>
 
-            {/* <div className="flex flex-row  items-center">
+      {/* <div className="flex flex-row  items-center">
                 <strong className="">UsuarioADM</strong>
                 <div className="bg-green-500 h-12 w-12 flex items-center  justify-center pl-1 rounded-full ml-auto ">
                     {/* footo do  user */}
-            {/* </div> */}
-            {/* // <ChevronDown size={20} />
+      {/* </div> */}
+      {/* // <ChevronDown size={20} />
                 // <MoonIcon size={20} />
             // </div> */}
-        </header>
-    );
+    </header>
+  );
 };
--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -1,79 +1,73 @@
 type Comprinhas = {
-    id: number;
-    nomeCliente: string;
-    status: "Em Processo" | "Pago" | "Transação Com Error";
-    data: string;
+  id: number;
+  nomeCliente: string;
+  status: "Em Processo" | "Pago" | "Transação Com Error";
+  data: string;
 };
 const compra: Comprinhas[] = [
-    {
-        id: 1,
-        nomeCliente: "João",
-        status: "Transação Com Error",
-        data: "12/01/2022",
-    },
-    {
-        id: 2,
-        nomeCliente: "Gustavo",
-        status: "Transação Com Error",
-        data: "03/09/2021",
-    },
-    {
-        id: 3,
-        nomeCliente: "Caua",
-        status: "Transação Com Error",
-        data: "10/10/2020",
-    },
+  {
+    id: 1,
+    nomeCliente: "João",
+    status: "Transação Com Error",
+    data: "12/01/2022",
+  },
+  {
+    id: 2,
+    nomeCliente: "Gustavo",
+    status: "Transação Com Error",
+    data: "03/09/2021",
+  },
+  {
+    id: 3,
+    nomeCliente: "Caua",
+    status: "Transação Com Error",
+    data: "10/10/2020",
+  },
 ];
 
 const coluns = ["Checkbox", "Cliente", "Status", "Data", "Opções"];
 
 export const Table = () => {
-    return (
-        <table className="flex-col text-sm text-gray-500 dark:text-gray-400 mt-12 md:w-11/12  shadow-2xl">
-            <thead className="border">
-                <tr className="flex text-xs text-indigo-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-indigo-300 justify-between">
-                    {coluns.map((colun) => {
-                        return (
-                            <th
-                                key={colun}
-                                className="flex w-1/6 justify-center p-2"
-                            >
-                                {colun}
-                            </th>
-                        );
-                    })}
-                </tr>
-            </thead>
+  return (
+    <table className="flex-col text-sm text-gray-500 dark:text-gray-400 mt-12 md:w-11/12  shadow-2xl">
+      <thead className="border">
+        <tr className="flex text-xs text-indigo-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-indigo-300 justify-between">
+          {coluns.map((colun) => {
+            return (
+              <th key={colun} className="flex w-1/6 justify-center p-2">
+                {colun}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
 
-            <tbody className="border rounded-xl">
-                {compra.map((compra) => {
-                    return (
-                        <tr
-                            key={compra.id}
-                            className="flex text-xs text-indigo-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-indigo-300 justify-between p-2"
-                        >
-                            <td className="flex w-1/6 justify-center p-2">
-                                <input
-                                    id="checkbox-table-search-1"
-                                    type="checkbox"
-                                    className=" text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                />
-                                <label
-                                    htmlFor="checkbox-table-search-1"
-                                    className=""
-                                ></label>
-                            </td>
-                            <td
-                                scope="row"
-                                className=" text-gray-900 dark:text-white justify-center flex w-1/6 p-2 font-bold"
-                            >
-                                {compra.nomeCliente}
-                            </td>
-                            <td className="text-gray-900 dark:text-white justify-center flex w-1/6 p-2  rounded-full">
-                                {compra.status}
-                            </td>
+      <tbody className="border rounded-xl">
+        {compra.map((compra) => {
+          return (
+            <tr
+              key={compra.id}
+              className="flex text-xs text-indigo-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-indigo-300 justify-between p-2"
+            >
+              <td className="flex w-1/6 justify-center p-2">
+                <input
+                  id="checkbox-table-search-1"
+                  type="checkbox"
+                  className=" text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label htmlFor="checkbox-table-search-1" className=""></label>
+              </td>
+              <td
+                scope="row"
+                className=" text-gray-900 dark:text-white justify-center flex w-1/6 p-2 font-bold"
+              >
+                {compra.nomeCliente}
+              </td>
+              <td className="text-gray-900 dark:text-white justify-center flex w-1/6 p-2  rounded-full">
+                {compra.status}
+              </td>
 
-                            {/* <td className="flex justify-center w-1/6 p-2">
+              {/* <td className="flex justify-center w-1/6 p-2">
                                 {Intl.NumberFormat("pt-BR", {
                                     style: "currency",
                                     currency: "BRL",
@@ -85,27 +79,25 @@ export const Table = () => {
                             <td className="flex w-1/6 justify-center p-2">
                                 {compra.quantidade}
                             </td> */}
-                            <td className="flex w-1/6 justify-center p-2">
-                                {compra.data}
-                            </td>
-                            <td className="flex w-1/6 justify-center p-2">
-                                <a
-                                    href="#"
-                                    className="font-medium text-indigo-600 dark:text-indigo-500 hover:underline"
-                                >
-                                    Edit
-                                </a>
-                                <a
-                                    href="#"
-                                    className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
-                                >
-                                    Remove
-                                </a>
-                            </td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-    );
+              <td className="flex w-1/6 justify-center p-2">{compra.data}</td>
+              <td className="flex w-1/6 justify-center p-2">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 dark:text-indigo-500 hover:underline"
+                >
+                  Edit
+                </a>
+                <a
+                  href="#"
+                  className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
+                >
+                  Remove
+                </a>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
