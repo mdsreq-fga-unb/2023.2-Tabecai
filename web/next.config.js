333e2018aefc4cd4647d1073c1daa7a29e12684c
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2023-11-23 01:51:18 -0300

    cors

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: 'ec2-15-228-243-0.sa-east-1.compute.amazonaws.com:3535',
+    API_URL: 'https://ec2-15-228-243-0.sa-east-1.compute.amazonaws.com:3535',
   },
   headers: async () => {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
